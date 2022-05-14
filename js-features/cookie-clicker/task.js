24f27eb6db816ceb494fe60920dd6cb0cce8441d
Tanya <voznyuk1998@mail.ru>
2022-05-14 20:16:47 +0300

    Доработки

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,11 +1,21 @@
 var clicks = document.getElementById("clicker__counter");
 var cookieID = document.getElementById("cookie");
 var clickscounter = 0; // счетчик
+var Clicked = 0;/// переменная времени щелчка
+var clicksTime = document.getElementById("clicker__counter1");
+
+ 
 cookieID.addEventListener("click", function() {
     clickscounter = clickscounter + 1;
     clicks.textContent =  clickscounter;  // изменение контента тега
-});// обработка событий(событие и действие)
 
+   
+   // var t = (new Date()).getSeconds();
+    //Clicked =  clickscounter / t  ;
+    //clicksTime.textContent = Clicked;
+   
+   
+});
 
 var bigsize = "300"; //Размер большой картинки
 var smallsize = "200"; //Размер маленькой картинки
--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,5 +1,20 @@
+///таймер на 60 сек 60-0
+let seconds = document.getElementById("timer");
+
+let interval = setInterval(function () { 
+
+	seconds.textContent -- ;
+
+if(seconds.textContent == 0) {
+ 	alert('Вы победили в конкурсе!');
+	clearInterval(interval);
+	}
+ 
+},1000)
+
+///попробовала реализовать доп задание получился только таймер обратного отсчета с конкретной установленной датой 
 document.addEventListener('DOMContentLoaded', () => {
-	const newprizedraw = new Date('jun 10 2022 1:24:00');
+	const newprizedraw = new Date('may 29 2022 1:24:00');
 	
 	const daysVal = document.querySelector('.time-count__days .time-count__val');
 	const hoursVal = document.querySelector('.time-count__hours .time-count__val');
