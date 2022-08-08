6b429faca9a2b5c8ad17a9e13c861ab79d7b21c2
Tanya <voznyuk1998@mail.ru>
2022-08-08 22:13:54 +0300

    finish auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,29 +1,35 @@
-const signin = document.getElementById("signin");
-signin.classList.add("signin_active");
-const welcome = document.getElementById(".welcome");// лучше использовать getElementById или же querySelector
+const welcome = document.getElementById("welcome");
 const idUser = document.getElementById("user_id");
 const form = document.getElementById("signin__form");
 
+function welcomeUser() {
+    form.classList.add("welcome");
+    welcome.classList.add("welcome_active");
+}
+
 form.addEventListener("submit", event => {
     event.preventDefault();
-    const data = new FormData(form);
 
     const xhr = new XMLHttpRequest();
     xhr.open("POST", 'https://netology-slow-rest.herokuapp.com/auth.php');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            if (response.success === true) {
-                signin.classList.remove("signin_active");
-                const userId = response["user_id"];
-                localStorage.setItem("id", userId);
-                welcome.classList.add("welcome_active");
-                idUser.textContent = userId;
-            } else {
-                alert("Неверный логин/пароль");
-            }
-            form.reset()
+
+    xhr.responseType = 'json';
+
+    const formData = new FormData(form);
+    xhr.send(formData);
+
+    xhr.onload = function () {
+
+        const response = xhr.response;
+
+        if (response.user_id) {
+            idUser.textContent = response.user_id;
+            welcomeUser();
+            localStorage.setItem("id", idUser.textContent);
+        } else {
+            alert("Неверный логин/пароль");
         }
+        form.reset()
     }
-    xhr.send(data);
 })
+
